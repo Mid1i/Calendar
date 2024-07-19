0a555704314449ec1447f86883730c67c9d6a52b
Mid1i <michac.tchernov@yandex.ru>
2024-07-19 15:18:36 +0300

    Fixed bug with commas

--- src/components/CalendarFooter.vue
+++ src/components/CalendarFooter.vue
@@ -16,28 +16,33 @@
 
 
 	const getTitle = computed<string>(() => {
-		const length: number = isPeriodAction(currentAction) ? props.selectedDates.filter((item) => item.from && item?.to).length : props.selectedDates.length;
+		const length: number = isPeriodAction(currentAction) ? getPeriodDatesLength.value : props.selectedDates.length;
 		const isSingle: boolean = length === 1;
+		console.log(isPeriodAction(currentAction) ?  getPeriodDatesLength.value - 1 : props.selectedDates.length - 1)
 		
 		if (length === 0) return "";
 
-		const titles: Record<TypeActions, () => string> = {
-			ONE_DATE: () => "Выбранная дата:",
-			SEVERAL_DATES: () => isSingle ? "Выбранная дата:" : `Выбраны ${length} ${getWordByAmount(length, "дата", "даты", "дат")}:`,
-			ONE_PERIOD: () => "Выбранный период:",
-			SEVERAL_PERIODS: () => `Выбраны ${length} ${getWordByAmount(length, "период", "периода", "периодов")}:`
+		const titles: Record<TypeActions, string> = {
+			ONE_DATE: "Выбранная дата:",
+			SEVERAL_DATES: isSingle ? "Выбранная дата:" : `Выбраны ${length} ${getWordByAmount(length, "дата", "даты", "дат")}:`,
+			ONE_PERIOD: "Выбранный период:",
+			SEVERAL_PERIODS: `Выбраны ${length} ${getWordByAmount(length, "период", "периода", "периодов")}:`
 		};
 
-		return titles[currentAction]();
+		return titles[currentAction];
 	});
 
 
-	const getSelectedDate = (date: ISelectedDates): string => {
-		if (!isPeriodAction(currentAction)) return getFullDate(date.from);
+	const getPeriodDatesLength = computed<number>(() => props.selectedDates.filter(item => item.from && item?.to).length);
 
+
+	const getSelectedDate = (date: ISelectedDates, index: number): string => {
+		if (!isPeriodAction(currentAction)) return `${getFullDate(date.from)}${index === props.selectedDates.length - 1 ? "" : ", "}`;
+		
 		if (date?.to) {
-			const periodLength = Math.ceil((date.to.getTime() - date.from.getTime()) / (1000 * 60 * 60 * 24)) + 1;
-			return `${getFullDate(date.from)}-${getFullDate(date.to)} (${periodLength} ${getWordByAmount(periodLength, "день", "дня", "дней")})`;
+			const periodLength: number = Math.ceil((date.to.getTime() - date.from.getTime()) / (1000 * 60 * 60 * 24)) + 1;
+			const getComma: string = index === getPeriodDatesLength.value - 1 ? "" : ", ";
+			return `${getFullDate(date.from)}-${getFullDate(date.to)} (${periodLength} ${getWordByAmount(periodLength, "день", "дня", "дней")})${getComma}`;
 		}
 
 		return "";
@@ -52,9 +57,12 @@
 			<li 
 				v-for="(date, index) in selectedDates"
 				:key="index"
-				:class="['footer__list-el', isPeriodAction(currentAction) && 'period']"
+				:class="[
+					'footer__list-el', 
+					isPeriodAction(currentAction) && 'period'
+				]"
 			>
-				{{ `${getSelectedDate(date)}${index === selectedDates.length - 1 ? "" : ","}` }}
+				{{ getSelectedDate(date, index) }}
 			</li>
 		</ul>
 	</footer>
@@ -71,19 +79,22 @@
 			align-items: center;
 			display: flex;
 			flex-wrap: wrap;
-			gap: 5px;
 
 			&-title {
 				font-size: 18px;
 				font-weight: 500;
+
+				margin-right: 10px;
 			}
 
 			&-el {
 				font-size: 16px;
+
+				margin-right: 5px;
 				width: 85px;
 
 				&.period {
-					width: 250px;
+					width: 260px;
 				}
 			}
 		}
